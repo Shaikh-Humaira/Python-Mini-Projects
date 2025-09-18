125011eac42b225aea9b3e9b952d0a1ee99f3d83
Shaikh Humaira <shaikhhumaira110@gmail.com>
2025-09-18 23:08:36 +0530

    Add files via upload

---
+++ 1-welcome-message-generator.py
@@ -0,0 +1,16 @@
+# 1. Welcome Message Generator: Variables and data types
+
+#user details
+name = input("What's your name? ")
+hobby = input("What's your favorite hobby? ")
+age = int(input("How old are you? "))
+color = input("What is your favorite color? ")
+
+#personalized welcome message
+print("\n--- Welcome Message ---")
+print(f"Hello, {name}! 👋")
+print(f"Welcome to the world of Python Programming.")
+print(f"It's great to know that you love {hobby}.")
+print(f"Get ready to build something amazing today.")
+print(f"You are {age} years old and {color} is a beautiful color!")
+print("You're now ready to start your Python adventure 🚀🐍")
